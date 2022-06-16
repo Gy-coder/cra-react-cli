3141a47449ae5f1c8e00d757beed9d5699900fe7
Gy <406631892@qq.com>
2022-06-17 00:37:17 +0800

    可以生成package.json

--- packages/create-react-app/createReactApp.js
+++ packages/create-react-app/createReactApp.js
@@ -1,8 +1,10 @@
 const { Command } = require("commander");
 const chalk = require("chalk");
 const packageJSON = require("./package.json");
+const path = require("path");
+const fs = require("fs-extra");
 
-function init() {
+async function init() {
   let projectName;
   new Command(packageJSON.name)
     .version(packageJSON.version)
@@ -13,6 +15,27 @@ function init() {
     })
     .parse(process.argv);
   console.log("projectName", projectName);
+  await createApp(projectName);
+}
+
+async function createApp(projectName) {
+  let root = path.resolve(projectName);
+  fs.ensureDir(root);
+  console.log(`creating a new React app  in ${chalk.green(root)}`);
+  const packageJSON = {
+    name: projectName,
+    version: "0.1.0",
+    private: true,
+  };
+  fs.writeFileSync(
+    path.join(root, "package.json"),
+    JSON.stringify(packageJSON, null, 2)
+  );
+  const originalDir = process.cwd();
+  process.chdir(root);
+  console.log("projectName", projectName);
+  console.log("root:", root);
+  console.log("originalDir:", originalDir);
 }
 
 module.exports = {
