c10795a135b34a23b1275f7d6a95a101ccc21019
Gy <406631892@qq.com>
2022-06-21 00:11:04 +0800

    初始化模版

--- packages/cra-template2/template/src/App.jsx
+++ packages/cra-template2/template/src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  return <h1>App</h1>;
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount(count + 1);
+  return (
+    <div className="app">
+      <h1 className="m">Welcome to banana React cli</h1>
+      <h2 className="m">try to click button</h2>
+      <p className="m">count: {count}</p>
+      <button onClick={handleClick} className="m">
+        +1
+      </button>
+    </div>
+  );
 };
 
 export default App;
