4c6fb12362be15d4a54ce3b3442473a490665ce5
Gy <406631892@qq.com>
2022-06-21 21:09:00 +0800

    eject可以抛出config和scripts

--- packages/react-scripts2/config/paths.js
+++ packages/react-scripts2/config/paths.js
@@ -5,10 +5,15 @@ const appDictionary = fs.realpathSync(process.cwd());
 const resolveProject = (relativePath) =>
   path.resolve(appDictionary, relativePath);
 
+const resolveOwn = (relativePath) =>
+  path.resolve(__dirname, "..", relativePath);
+
 module.exports = {
   appHtml: resolveProject("public/index.html"),
   appBuild: resolveProject("build"),
   appIndexJs: resolveProject("src/index.js"),
   appPublic: resolveProject("public"),
   appSrc: resolveProject("src"),
+  ownPath: resolveOwn("."),
+  appPath: resolveProject("."),
 };
--- packages/react-scripts2/scripts/eject.js
+++ packages/react-scripts2/scripts/eject.js
@@ -1,3 +1,49 @@
-console.log(`
-    ✨✨     eject
-`);
+const { prompt } = require("inquirer");
+const chalk = require("chalk");
+const path = require("path");
+const fs = require("fs-extra");
+const paths = require("../config/paths");
+
+prompt([
+  {
+    type: "confirm",
+    message: `${chalk.cyan(
+      "Are you sure you want to eject? This action is permanent."
+    )}`,
+    name: "shouldEject",
+    initial: false,
+  },
+]).then((answer) => {
+  if (!answer.shouldEject)
+    return console.log(`${chalk.cyan("Close one! Eject aborted.")}`);
+  const ownPath = paths.ownPath;
+  const appPath = paths.appPath;
+  const folders = ["config", "scripts"];
+
+  const files = folders.reduce((files, folder) => {
+    return files.concat(
+      fs
+        .readdirSync(path.join(ownPath, folder))
+        .map((file) => path.join(ownPath, folder, file))
+        .filter((file) => fs.lstatSync(file).isFile())
+    );
+  }, []);
+
+  console.log(chalk.cyan(`Copying files into ${appPath}`));
+
+  folders.forEach((folder) => {
+    fs.mkdirSync(path.join(appPath, folder), { recursive: true });
+  });
+
+  files.forEach((file) => {
+    let content = fs.readFileSync(file, "utf8");
+
+    // Skip flagged files
+    content = content.trim() + "\n";
+    console.log(
+      `  Adding ${chalk.cyan(file.replace(ownPath, ""))} to the project`
+    );
+    fs.writeFileSync(file.replace(ownPath, appPath), content);
+  });
+  console.log();
+});
